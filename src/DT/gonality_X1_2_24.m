16bf18d9f87e2d7ea31c4521cef657dc966f66ec
Marin Varivoda <mvarivoda.stype@gmail.com>
2026-05-18 13:55:43 +0200

    fix X1_2_24 calculation, use different prime

--- src/DT/gonality_X1_2_24.m
+++ src/DT/gonality_X1_2_24.m
@@ -7,7 +7,7 @@ C := Curve(A, X);
 C_proj := ProjectiveClosure(C);
 
 // Reduce the curve over finite field
-p := 5;
+p := 7; // Note that gon_5 = 7, while gon_7 = 8
 C_fin := Curve(Reduction(C_proj, p));
 
 printf "Initialized the model for X_1(%o, %o) and reduced it over a finite field with %o elements.\n", m, m*n, p;
@@ -16,7 +16,7 @@ printf "Computing the exact gonality of X_1(%o, %o)... (this might take a moment
 T := Time();
 gonality := Gonality(C_fin);
 
-printf "Calculation complete. Gonality: %o\n", gonality;
+printf "Calculation complete. Gonality: %o\n", gonality; // 8
 printf "Total calculation time: %o seconds\n", Time(T);
 
 exit;
